f99007748d1f01bc36c733ff2a6a0d7038ad88f5
Stephen George <stephen.george@ibm.com>
2021-06-08 22:18:16 -0500

    building out sad path

--- src/test/groovy/EventSadPath.groovy
+++ src/test/groovy/EventSadPath.groovy
@@ -37,4 +37,50 @@ class EventSadPath extends Specification {
         assert e.response.status == 415: 'invalidly retrieved events while using incorrect content type'
         //can use Data Driven Testing to test multiple different incorrect content types for extensive coverage
     }
+
+    def 'user cant get events for a network of repositories with invalid header authentication'() {
+        when:
+        def response = client.get(path: "networks/octocat/hello-world/events",
+                headers: ["User-Agents": "StephenG453"],
+                requestContentType: 'application/json'
+        )
+        then:
+        HttpResponseException e = thrown(HttpResponseException)
+        assert e.response.status == 403: 'invalidly retrieved events while using incorrect header'
+        //can use Data Driven Testing to test multiple different incorrect headers for extensive coverage
+    }
+
+    def 'user cant get events for a network of repositories with invalid header content type'() {
+        when:
+        def response = client.get(path: "networks/octocat/hello-world/events",
+                requestContentType: TEXT,
+                headers: ["User-Agent": "StephenG453"]
+        )
+        then:
+        HttpResponseException e = thrown(HttpResponseException)
+        assert e.response.status == 415: 'invalidly retrieved events while using incorrect content type'
+        //can use Data Driven Testing to test multiple different incorrect content types for extensive coverage
+    }
+
+    def 'user cant get events for a network of repositories while specifying an invalid owner'() {
+        when:
+        def response = client.get(path: "networks/ownerthatdoesntexist/hello-world/events",
+                requestContentType: 'application/json',
+                headers: ["User-Agent": "StephenG453"]
+        )
+        then:
+        HttpResponseException e = thrown(HttpResponseException)
+        assert e.response.status == 404: 'invalidly retrieved events while using owner that doesnt exist'
+    }
+
+    def 'user cant get events for a network of repositories while specifying an invalid repository'() {
+        when:
+        def response = client.get(path: "networks/octocat/repothatdoesntexist/events",
+                requestContentType: 'application/json',
+                headers: ["User-Agent": "StephenG453"]
+        )
+        then:
+        HttpResponseException e = thrown(HttpResponseException)
+        assert e.response.status == 404: 'invalidly retrieved events while using a repository that doesnt exist'
+    }
 }
