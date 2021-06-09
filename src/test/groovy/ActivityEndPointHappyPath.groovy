ae5d8bc352ac36affdc103db40c0af5e3dfc1cc6
Stephen George <stephen.george@ibm.com>
2021-06-08 22:11:55 -0500

    building out happy path

--- src/test/groovy/ActivityEndPointHappyPath.groovy
+++ src/test/groovy/ActivityEndPointHappyPath.groovy
@@ -8,7 +8,6 @@ class ActivityEndPointHappyPath extends Specification {
 
     def setupSpec() {
         client = new RESTClient('https://api.github.com/')
-//        firstClientData = new FirstClientData()
     }
 
     def 'user can get events'() {
@@ -17,12 +16,26 @@ class ActivityEndPointHappyPath extends Specification {
                 headers: ["User-Agent": "StephenG453"],
                 requestContentType: 'application/json'
         )
-
         System.out.println("data:\n" + response.data)
         System.out.println("headers:\n" + response.headers)
         then:
-        assert response.status == 200: 'could not retrieve event'
+        assert response.status == 200: 'could not retrieve events'
+        assert response.contentType == 'application/json': 'invalid content type specified'
+        //if I knew more about this API to know exactly what the data in the response means we could do
+        // multiple assertions regarding that
     }
 
+    def 'user can get events for a network of repositories'() {
+        when:
+        def response = client.get(path: "networks/octocat/hello-world/events",
+                headers: ["User-Agent": "StephenG453"],
+                requestContentType: 'application/json'
+        )
+        then:
+        assert response.status == 200: 'could not retrieve events for a network of repositories'
+        assert response.contentType == 'application/json': 'invalid content type specified'
+        //if I knew more about this API to know exactly what the data in the response means we could do
+        // multiple assertions regarding that
+    }
 
 }
